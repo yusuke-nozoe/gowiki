88569b1edfc8a47fa972ad704d50ca3480bf77e6
Yusuke Nozoe <yusuke.nozoe@gmail.com>
2015-09-17 22:16:23 +0900

    Add more actions

--- wiki.go
+++ wiki.go
@@ -45,8 +45,24 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textara><br>"+
+		"<input type=\"submit\" value=\"Save\" />"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
 func main() {
 	//http.HandleFunc("/", handler)
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
